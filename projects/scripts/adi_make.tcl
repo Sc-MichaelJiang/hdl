2c90edd91ac5e2f3e35f771eeeca9dfa640bf529
AndreiGrozav <andrei.grozav@analog.com>
2018-12-05 15:57:35 +0000

    restructure adi_make.tcl

--- projects/scripts/adi_make.tcl
+++ projects/scripts/adi_make.tcl
@@ -36,106 +36,26 @@
 ##############################################################################
 ## The folowing procedures are available:
 ##
-## adi_make_lib <args>
+## adi_make::lib <args>
 ##               -"all"(project libraries)
 ##               -"library name to build (plus path to it relative to library folder)
 ##                  e.g.: adi_make_lib xilinx/util_adxcvr
-## adi_make_boot_bin - expected that u-boot*.elf (plus bl31.elf for zynq_mp)
+## adi_make::boot_bin - expected that u-boot*.elf (plus bl31.elf for zynq_mp)
 ##                     files are in the project folder"
 ## For more info please see: https://wiki.analog.com/resources/fpga/docs/build
 
 
-# library build procedure
-proc adi_make_lib { libraries } {
 
-  variable ad_make_env::library_dir
-  variable ad_make_env::build_list
-  variable ad_make_env::PWD
-
-  if { $libraries == "all" } {
-    ad_make_env::get_local_lib
-    set libraries $build_list
-  }
-
-  set build_list ""
-  set space " "
-  puts "Building:"
-  foreach b_lib $libraries {
-    puts "- $b_lib"
-    append build_list $library_dir/$b_lib$space
-  }
-
-  puts "Please wait, this might take a few minutes"
-  ad_make_env::init_build $build_list ;# run in command line mode, with arguments
-  cd $PWD
-}
-
-# boot_bin build procedure
-proc adi_make_boot_bin {} {
-
-  variable ad_make_env::root_hdl_folder
-  set arm_tr_sw_elf "bl31.elf"
-  set boot_bin_folder "boot_bin"
-  set uboot_elf "u-boot.elf"
-  catch { set uboot_elf "[glob "./u-boot*.elf"]" } err
-  catch { set hdf_file "[glob "./*.sdk/system_top.hdf"]" } err
-
-  puts "root_hdl_folder $root_hdl_folder"
-  puts "uboot_elf $uboot_elf"
-  puts "hdf_file $hdf_file"
-
-  # Xilinx SDK
-  package require platform
-  set os_type [platform::generic]
-  if { [regexp ^win $os_type] } {
-      set w_cmd where
-  } elseif { [regexp ^linux $os_type] } {
-      set w_cmd which
-  } else {
-    puts "ERROR: Unknown OS: $os_type"
-    return
-  }
-  set xsct_loc [exec $w_cmd xsct]
-
-  # search for Xilinx Command Line Tool (SDK)
-  if { $xsct_loc == "" } {
-     puts $env(PATH)
-     puts "ERROR: SDK not installed or it is not defined in the enviroment path"
-     return
-  }
-
-  set xsct_script "exec xsct $root_hdl_folder/projects/scripts/adi_make_boot_bin.tcl"
-  set build_args "$hdf_file $uboot_elf $boot_bin_folder $arm_tr_sw_elf"
-  puts "Please wait, this may take a few moments."
-  eval $xsct_script $build_args
-}
-
-namespace eval ad_make_env {
+namespace eval adi_make {
   ##############################################################################
-  # to print build step messages "set msg_level=1" (set ad_make_env::msg_level 1)
+  # to print debug step messages "set msg_level=1" (set adi_make::msg_level 1)
   variable msg_level 0
   ##############################################################################
 
-  # global variables
-  variable build_list
   variable library_dir
-  variable PWD
+  variable PWD [pwd]
   variable root_hdl_folder
-  variable done_list
-  variable serch_pattern
-
-
-  # init local namespace variables
-  set build_list ""
-  set match ""
-  set PWD [pwd]
-  set done_list ""
-  set depend_lib ""
-  set match ""
-
-  # define library dependency search (Makefiles)
-  set serch_pattern "XILINX_.*_DEPS.*="
-
+  variable done_list ""
 
   # get library absolute path
   set root_hdl_folder ""
@@ -149,21 +69,20 @@ namespace eval ad_make_env {
 
   set library_dir "$root_hdl_folder/library"
 
-  ##############################################################################
+  #----------------------------------------------------------------------------
   # have debug messages
-  proc puts_msg_level { message } {
+  proc puts_msg { message } {
     variable msg_level
     if { $msg_level == 1 } {
       puts $message
     }
   }
 
-  ##############################################################################
+  #----------------------------------------------------------------------------
   # search for project IP dependencies
-  proc get_local_lib {} {
+  proc get_prj_dependencies {} {
 
-    variable library_dir
-    variable build_list
+    set build_list ""
 
     set search_pattern "LIB_DEPS.*="
     set match ""
@@ -177,93 +96,50 @@ namespace eval ad_make_env {
       if { $match != "" } {
         regsub -all $search_pattern $line "" library
         set library [string trim $library]
-        puts_msg_level "    - dependency library $library"
+        puts_msg "    - dependency library $library"
         append build_list "$library "
         set match ""
       }
     }
+    return $build_list
   }
 
-  ##############################################################################
-  # search for library IP dependencies
-  proc search_ip_dependency { path } {
+  #----------------------------------------------------------------------------
+  proc lib { libraries } {
 
-    # global variables
-    variable serch_pattern
     variable library_dir
-
-    puts_msg_level "DEBUG search_ip_dependency proc"
-
-    set match ""
-    set fp1 [open $library_dir/$path/Makefile r]
-    set file_data [read $fp1]
-    close $fp1
-
-    set lines [split $file_data \n]
-    foreach line $lines {
-      regexp $serch_pattern $line match
-      if { $match != "" } {
-        regsub -all $serch_pattern $line "" lib_dep
-        set lib_dep [string trim $lib_dep]
-        puts_msg_level "    > dependency library $lib_dep"
-        # build dependency
-        build_dep_lib $lib_dep
-        set match ""
-      }
-    }
-  }
-
-  ##############################################################################
-  # build procedure
-  proc build_dep_lib { library } {
-
-    # global variables
+    variable PWD
     variable done_list
-    variable library_dir
 
-    puts_msg_level "DEBUG build_dep_lib proc"
-
-    # determine if the IP was previously built in the current adi_make_lib.tcl call
-    if { [regexp $library $done_list] } {
-      puts_msg_level "Build previously done on $library"
-      return
-    } else {
-      puts_msg_level "- Start build of $library"
+    set build_list $libraries
+    if { $libraries == "all" } {
+      set build_list "[get_prj_dependencies]"
     }
-    puts_msg_level "- Search dependencies for $library"
 
-    # search for current IP dependencies
-    search_ip_dependency $library
+    set libraries ""
+    puts "Building:"
+    foreach b_lib $build_list {
+      puts "- $b_lib"
+      append libraries "$library_dir/$b_lib "
+    }
 
-    puts_msg_level "- Continue build on $library"
-    set ip_name "[file tail $library]_ip"
+    puts "Please wait, this might take a few minutes"
 
-    cd $library_dir/${library}
-    exec vivado -mode batch -source "$library_dir/${library}/${ip_name}.tcl"
-    file copy -force ./vivado.log ./${ip_name}.log
-    puts "- Done building $library"
-    append done_list $library
-  }
-
-  ##############################################################################
-  # search for sub dir ips and start build
-  proc init_build { ips } {
-
-    set makefiles ""
-    # searching for subdir libraries in path for first argument
-    set first_lib [lindex $ips 0]
+    # searching for subdir libraries in path of the given args
+    set first_lib [lindex $libraries 0]
     if { $first_lib == "" } {
      set first_lib "."
     }
-    # getting all parsed arguments (libraries)
+    # getting all (libraries)
     set index 0
     set library_element(1) $first_lib
-    foreach argument $ips {
+    foreach argument $libraries {
      incr index 1
      set library_element($index) $argument
     }
 
     # search for all possible IPs in the given argument paths
+    set makefiles ""
     if { $index == 0 } {
       set index 1
     }
@@ -300,10 +176,120 @@ namespace eval ad_make_env {
         set fs [string trim $fs]
       }
       regsub .*library/ $fs "" fs
-      build_dep_lib $fs
+      build_ip_dep $fs
     }
+
+    cd $PWD
+    set done_list ""
   }
-} ;# ad_make_env namespace
+
+  #----------------------------------------------------------------------------
+  # IP build procedure
+  proc build_ip_dep { library } {
+
+    variable done_list
+    variable library_dir
+
+    puts_msg "DEBUG build_ip_dep proc"
+
+
+    # determine if the IP was previously built in the current adi_make_lib.tcl call
+    if { [regexp $library $done_list] } {
+      puts_msg "Build previously done on $library"
+      return
+    } else {
+      puts_msg "- Start build of $library"
+    }
+    puts_msg "- Search dependencies for $library"
+
+    # search for current IP dependencies
+
+    # define library dependency search (Makefiles)
+    set serch_pattern "XILINX_.*_DEPS.*="
+    set dep_list ""
+
+    set match ""
+    set fp1 [open $library_dir/$library/Makefile r]
+    set file_data [read $fp1]
+    close $fp1
+
+    set lines [split $file_data \n]
+    foreach line $lines {
+      regexp $serch_pattern $line match
+      if { $match != "" } {
+        regsub -all $serch_pattern $line "" lib_dep
+        set lib_dep [string trim $lib_dep]
+        puts "    > dependency library $lib_dep"
+        append dep_list "$lib_dep "
+        set match ""
+      }
+    }
+
+    foreach lib $dep_list {
+      build_ip_dep $lib
+    }
+
+    puts_msg "- Continue build on $library"
+    set ip_name "[file tail $library]_ip"
+
+    cd $library_dir/${library}
+    exec vivado -mode batch -source "$library_dir/${library}/${ip_name}.tcl"
+    file copy -force ./vivado.log ./${ip_name}.log
+    puts "- Done building $library"
+    append done_list $library
+  }
+
+  #----------------------------------------------------------------------------
+  # boot_bin build procedure
+  proc boot_bin {} {
+
+    variable root_hdl_folder
+
+    set arm_tr_sw_elf "bl31.elf"
+    set boot_bin_folder "boot_bin"
+    if {[catch {set hdf_file "[glob "./*.sdk/system_top.hdf"]"} fid]} {
+      puts stderr "ERROR: $fid\n\rNOTE: you must have built hdl project\n\
+      \rSee: https://wiki.analog.com/resources/fpga/docs/build\n"
+      return
+    if {[catch {set uboot_elf "[glob "./u-boot*.elf"]" } fid]} {
+      puts stderr "ERROR: $fid\n\rNOTE: you must have a the u-boot.elf in [pwd]\n\
+      \rSee: https://wiki.analog.com/resources/fpga/docs/build\n"
+      return
+    }
+
+   }
+
+    puts "root_hdl_folder $root_hdl_folder"
+    puts "uboot_elf $uboot_elf"
+    puts "hdf_file $hdf_file"
+
+    # determine if Xilinx SDK tools are set in the enviroment
+    package require platform
+    set os_type [platform::generic]
+    if { [regexp ^win $os_type] } {
+      set w_cmd where
+    } elseif { [regexp ^linux $os_type] } {
+      set w_cmd which
+    } else {
+      puts "ERROR: Unknown OS: $os_type"
+      exit 1
+    }
+    set xsct_loc [exec $w_cmd xsct]
+
+    # search for Xilinx Command Line Tool (SDK)
+    if { $xsct_loc == "" } {
+      puts $env(PATH)
+      puts "ERROR: SDK not installed or it is not defined in the enviroment path"
+      exit 1
+    }
+
+    set xsct_script "exec xsct $root_hdl_folder/projects/scripts/adi_make_boot_bin.tcl"
+    set build_args "$hdf_file $uboot_elf $boot_bin_folder $arm_tr_sw_elf"
+    puts "Please wait, this may take a few moments."
+    eval $xsct_script $build_args
+  }
+
+} ;# ad_make namespace
 
 
 #############################################################################
